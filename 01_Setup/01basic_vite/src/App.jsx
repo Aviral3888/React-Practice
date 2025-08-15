c73896ea10c9c5fec3b7fa44864f992745e8f082
Aviral Goel <avigoel.3888@gmail.com>
2025-08-15 15:52:06 +0530

    React: vite - component creation ways

--- 01_Setup/01basic_vite/src/App.jsx
+++ 01_Setup/01basic_vite/src/App.jsx
@@ -1,12 +1,14 @@
 import Youtube from "./Youtube"
 
 function App() {
+  const username = "Amit";
   return (
     <>
       <h3>
-        Vite react app
+        Vite react app {2 + 2}
       </h3>
-      <Youtube/>
+      <p>Name: {username}</p>
+      <Youtube />
     </>
   )
 }
--- 01_Setup/01basic_vite/src/main.jsx
+++ 01_Setup/01basic_vite/src/main.jsx
@@ -1,9 +1,44 @@
-import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://www.google.com',
+    target: '_blank'
+  },
+  children: `<h3>Click Me to visit Google</h3>`
+}
+
+const ActualReactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',
+    target: '_blank'
+  },
+  'Click Me to visit Google'
+)
+
+// This works when used as <MyApp /> under render()
+function MyApp() {
+  return (
+    <>
+      <div>
+        <h3>Custom App</h3>
+      </div>
+    </>
+  )
+}
+
+// This also works when used directly as AnotherElement under render()
+const AnotherElement = (
+  <a href="https://www.google.com" target='_blank'>Visit Google</a>
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
+  // <MyApp />
+  // AnotherElement
+  // ActualReactElement 
 )
