fbc838f48aa5ae173ca73a56ab9d3ac434094cd2
Aviral Goel <avigoel.3888@gmail.com>
2025-08-17 00:43:07 +0530

    React: Bg toggle app - features updated

--- 02_Practice/04_bgChanger/src/App.jsx
+++ 02_Practice/04_bgChanger/src/App.jsx
@@ -11,26 +11,41 @@ function App() {
   //   setColor(color);
   // }
 
-  return (
-    <div className="w-full h-screen duration-200 p-0" style={{ backgroundColor: color }}>
-      <div className="flex flex-col flex-wrap justify-center top-12 inset-x-0 px-2 py-12">
-        <h2 className="self-center text-2xl bg-gray-500 p-3 pl-5 pr-5 rounded-md mb-12 w-fit hover:bg-gray-700 hover:text-white hover:cursor-pointer">Background Changer</h2>
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl w-fit self-center">
-          <button
-            className='outline-none px-4 py-1 rounded-full shadow-lg text-black bg-red-400'
-            onClick={() => setColor('#FF4444')}
-          >Red</button>
-          <button
-            className='outline-none px-4 py-1 rounded-full shadow-lg text-black bg-green-400'
-            onClick={() => setColor('#00CC66')}
-          >Green</button>
-          <button
-            className='outline-none px-4 py-1 rounded-full shadow-lg text-black bg-blue-400'
-            onClick={() => setColor('#3399FF')}
-          >Blue</button>
-        </div>
-      </div>
+  const colors = [
+    { text: "Red", color: "#FF4444" },
+    { text: "Green", color: "#00CC66" },
+    { text: "Blue", color: "#3399FF" },
+    { text: "Purple", color: "#9933FF" },
+    { text: "Orange", color: "#FF9933" }
+  ];
 
+  return (
+    <div className="w-full h-screen duration-200 p-0 flex flex-col justify-between py-12" style={{ backgroundColor: color }}>
+      <div className="flex flex-col flex-wrap justify-center inset-x-0 px-2 gap-8">
+        <h2
+          className="shadow-lg rounded-3xl w-fit self-center text-lg p-3 pl-5 pr-5 bg-gray-500 hover:bg-gray-700 hover:cursor-pointer">
+          Background Color - Toggle
+        </h2>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl w-fit self-center">
+          {
+            colors.map(({ text, color }) => (
+              <button
+                key={color}
+                className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
+                style={{ backgroundColor: color }}
+                onClick={() => setColor(color)}
+              >{text}</button>
+            ))
+          }
+        </div>
+        <p className="w-fit self-center text-white">Current Color: <span className='outline-none px-4 py-1 rounded-full shadow-lg' style={{color: color, backgroundColor: "black"}}>{color}</span></p>
+      </div>
+      <div className='flex flex-wrap justify-center bottom-2'>
+        <button
+          className='text-md rounded-full w-fit px-5 py-1 shadow-lg bg-black text-white'
+          onClick={() => setColor('#2b2b2b')}
+        >Reset</button>
+      </div>
     </div>
   )
 }
